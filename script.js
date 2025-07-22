71932f31fb56b1b15a52787839a38a232a821641
penguinissus <jocelyn.ty.lau@gmail.com>
2025-07-22 14:58:21 -0400

    css and js for rotating words

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+//toggle thing for menu
 const menu = document.querySelector("#mobile-menu")
 const menuLinks = document.querySelector('.navbar__menu')
 
 menu.addEventListener("click", function () {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle("active");
-})
+});
+
+//Function to make words move
+const logos = document.querySelector(".rotating-logos__track").cloneNode(true);
+document.querySelector(".rotating-logos").appendChild(logos);
